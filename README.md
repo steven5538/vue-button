c8298065c2a8b80213d3c091b425bfbcfc4012d5
steven5538 <steven5538@gmail.com>
2016-11-22 11:01:42 +0800

    add test.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# vue-button [![Build Status](https://travis-ci.org/steven5538/vue-button.svg?branch=master)](https://travis-ci.org/steven5538/vue-button) [![Code Coverage](https://img.shields.io/codeclimate/coverage/github/steven5538/vue-button.svg)](https://codeclimate.com/github/steven5538/vue-button) [![Dependency Status](https://gemnasium.com/badges/github.com/steven5538/vue-button.svg)](https://gemnasium.com/github.com/steven5538/vue-button) ![MIT License](https://img.shields.io/github/license/steven5538/vue-button.svg)
+# vue-button [![Build Status](https://travis-ci.org/steven5538/vue-button.svg?branch=master)](https://travis-ci.org/steven5538/vue-button) [![Dependency Status](https://gemnasium.com/badges/github.com/steven5538/vue-button.svg)](https://gemnasium.com/github.com/steven5538/vue-button) ![MIT License](https://img.shields.io/github/license/steven5538/vue-button.svg)
 
 
 > Button component for Vue.js v2.0.1+.
@@ -10,9 +10,14 @@
 $ npm install vue-button
 ```
 ### manual
-Just download `dist/vue-button.js` and include it in your HTML file:
+Download `dist/vue-button.js` and include it in your HTML file:
 ```html
 <script src="path/to/vue-button/dist/vue-button.js"></script>
+```
+or you can include from [unpkg](https://unpkg.com).
+```html
+<!--use the lastest release-->
+<script src="https://unpkg.com/vue-button@lastest"></script>
 ```
 ## Usage
 ```html
--- test/unit/specs/Button.spec.js
+++ test/unit/specs/Button.spec.js
@@ -4,11 +4,41 @@ import VueButton from 'src/components/Button.vue'
 Vue.component('v-button', VueButton)
 
 describe('Button.vue', () => {
-  it('should render correct contents', () => {
+  it('render correct default class', () => {
     const vm = new Vue({
       template: '<div><v-button>Hello</v-button></div>',
       components: { VueButton }
     }).$mount()
-    expect(vm.$el.querySelector('.v-btn')).to.exist
+    expect(vm.$el.querySelector('button').className).to.equal('v-btn')
+  })
+
+  it('render correct default markup', () => {
+    const vm = new Vue({
+      template: '<div><v-button>Hello</v-button></div>',
+      components: { VueButton }
+    }).$mount()
+    expect(vm.$el.querySelector('button').getAttribute('markup')).to.equal('default')
+  })
+
+  it('render correct markup', () => {
+    const vm = new Vue({
+      template: '<div><v-button markup="primary">Hello</v-button></div>',
+      components: { VueButton }
+    }).$mount()
+    expect(vm.$el.querySelector('button').getAttribute('markup')).to.equal('primary')
+  })
+
+  it('render custom attribute', () => {
+    const vm = new Vue({
+      template: '<div><v-button large="true" disabled="true" class="test" id="test" name="test" markup="warning">Hello</v-button></div>',
+      components: { VueButton }
+    }).$mount()
+    console.log(vm.$el.querySelector('button'))
+    expect(vm.$el.querySelector('button').getAttribute('markup')).to.equal('warning')
+    expect(vm.$el.querySelector('button').getAttribute('large')).to.equal('true')
+    expect(vm.$el.querySelector('button').getAttribute('disabled')).to.equal('disabled')
+    expect(vm.$el.querySelector('button').getAttribute('class')).to.equal('v-btn test')
+    expect(vm.$el.querySelector('button').getAttribute('id')).to.equal('test')
+    expect(vm.$el.querySelector('button').getAttribute('name')).to.equal('test')
   })
 })
